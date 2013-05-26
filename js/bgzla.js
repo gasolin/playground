87a4f109e974a494f4a3bff8894b2b2c1ac73fab
gasolin <gasolin@gmail.com>
2013-05-26 15:40:11 +0800

    add toggle

--- js/bgzla.js
+++ js/bgzla.js
@@ -32,8 +32,32 @@ teams['kkuo'] = [
 ]
 */
 
-var bugzilla = bz.createClient();
-/*bugzilla.getBug(678223, function(error, bug) {
+// init
+jQuery(document).ready(function($) {
+  // Stuff to do as soon as the DOM is ready;
+  var bugzilla = bz.createClient();
+  $('#tef_zone').bind('touchstart click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    if (event.handled !== true) {
+      $('#tef_panel').toggle();
+      event.handled = true;
+    } else {
+      return false;
+    }
+  });
+  $('#leo_zone').bind('touchstart click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    if (event.handled !== true) {
+      $('#leo_panel').toggle();
+      event.handled = true;
+    } else {
+      return false;
+    }
+  });
+
+  /*bugzilla.getBug(678223, function(error, bug) {
 if (!error) {
   $('#panel').text(bug.summary);
 }
@@ -44,21 +68,34 @@ var tef_bugs;
 bugzilla.searchBugs({
     'username': 'autonome+bztest@gmail.com',
     'password': 'bztest1A',
-    'bug_status': ["NEW", "UNCONFIRMED", "ASSIGNED", "REOPENED", "READY"],
+    'bug_status': ['NEW', 'UNCONFIRMED', 'ASSIGNED', 'REOPENED', 'READY'],
     'field0-0-0': 'cf_blocking_b2g',
     'type0-0-0': 'contains',
     'value0-0-0': 'tef+',
     'field1-0-0': 'whiteboard',
     'type1-0-0': 'not_contains',
     'value1-0-0': 'npotb',
+    'component': ['Gaia', 'Gaia::Bluetooth File Transfer', 'Gaia::Browser',
+                  'Gaia::Calculator', 'Gaia::Camera', 'Gaia::Clock',
+                  'Gaia::Contacts', 'Gaia::Cost Control', 'Gaia::Dialer',
+                  'Gaia::E-Mail', 'Gaia::Everything.me',
+                  'Gaia::First Time Experience', 'Gaia::FMRadio',
+                  'Gaia::Gallery', 'Gaia::Homescreen',
+                  'Gaia::Keyboard', 'Gaia::Music', 'Gaia::PDF Viewer',
+                  'Gaia::Settings', 'Gaia::SMS', 'Gaia::System::Lockscreen',
+                  'Gaia::Video', 'General'
+                 ],
+    'product': 'Boot2Gecko'
   }, function(error, bugs) {
 if (!error) {
-	tef_bugs = bugs;
-	// console.log(bugs);
-	var outcome = '<ul>';
-  for(var i = 0; i < bugs.length; i++){
-  	outcome += '<li>' + '[<a href="http://bugzil.la/' + bugs[i].id + '">' + bugs[i].id + '</a>] ' + bugs[i].summary + '</li>\n'
-  };
+  tef_bugs = bugs;
+  $('#tef_panel').hide();
+  // console.log(bugs);
+  var outcome = '<ul>';
+  for (var i = 0; i < bugs.length; i++) {
+    outcome += '<li>' + '[<a href="http://bugzil.la/' + bugs[i].id + '">' +
+               bugs[i].id + '</a>] ' + bugs[i].summary + '</li>\n';
+  }
   outcome += '</ul>';
   $('#tef_panel').html(outcome);
   $('#tef_cnt').text(bugs.length);
@@ -70,26 +107,51 @@ var leo_bugs;
 bugzilla.searchBugs({
     'username': 'autonome+bztest@gmail.com',
     'password': 'bztest1A',
-    'bug_status': ["NEW", "UNCONFIRMED", "ASSIGNED", "REOPENED", "READY"],
+    'bug_status': ['NEW', 'UNCONFIRMED', 'ASSIGNED', 'REOPENED', 'READY'],
     'field0-0-0': 'cf_blocking_b2g',
     'type0-0-0': 'contains',
     'value0-0-0': 'leo+',
     'field1-0-0': 'whiteboard',
     'type1-0-0': 'not_contains',
     'value1-0-0': 'npotb',
+    'component': ['Gaia', 'Gaia::Bluetooth File Transfer', 'Gaia::Browser',
+                  'Gaia::Calculator', 'Gaia::Camera', 'Gaia::Clock',
+                  'Gaia::Contacts', 'Gaia::Cost Control', 'Gaia::Dialer',
+                  'Gaia::E-Mail', 'Gaia::Everything.me',
+                  'Gaia::First Time Experience', 'Gaia::FMRadio',
+                  'Gaia::Gallery', 'Gaia::Homescreen',
+                  'Gaia::Keyboard', 'Gaia::Music', 'Gaia::PDF Viewer',
+                  'Gaia::Settings', 'Gaia::SMS', 'Gaia::System::Lockscreen',
+                  'Gaia::Video', 'General'
+                 ],
+    'product': 'Boot2Gecko'
   }, function(error, bugs) {
 if (!error) {
-	leo_bugs = bugs;
-	// console.log(bugs);
-	var outcome = '<ul>';
-  for(var i = 0; i < bugs.length; i++){
-    outcome += '<li>' + '[<a href="http://bugzil.la/' + bugs[i].id + '">' + bugs[i].id + '</a>] ' + bugs[i].summary + '</li>\n'
-  };
+  leo_bugs = bugs;
+  $('#leo_panel').hide();
+  // console.log(bugs);
+  var outcome = '<ul>';
+  for (var i = 0; i < bugs.length; i++) {
+    outcome += '<li>' + '[<a href="http://bugzil.la/' + bugs[i].id + '">' +
+               bugs[i].id + '</a>] ' + bugs[i].summary + '</li>\n';
+  }
   outcome += '</ul>';
   $('#leo_panel').html(outcome);
   $('#leo_cnt').text(bugs.length);
 }
 });
+
+var now = Date();
+var startDay = 1; //0=sunday, 1=monday etc.
+var d = now.getDay(); //get the current day
+var weekStart = new Date(now.valueOf() -
+                (d <= 0 ?
+                        7 - startDay :
+                        d - startDay) * 86400000); //rewind to start day
+var weekEnd = new Date(weekStart.valueOf() +
+                       6 * 86400000); //add 6 days to get last day
+
+
 /*
 //blockers-tef-no-npotb-before-workweek
 {
@@ -138,3 +200,6 @@ if (!error) {
     'changed_after': '2013-04-14 11:00',
   }
   */
+});
+
+
