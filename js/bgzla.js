bda9a1792b1a683a3a37f8ebf3a5dcb17ce1895c
gasolin <gasolin@gmail.com>
2013-05-26 16:38:05 +0800

    add not assigned indicator

--- js/bgzla.js
+++ js/bgzla.js
@@ -31,6 +31,22 @@ teams['kkuo'] = [
   'vwang@mozilla.com',
 ]
 */
+function format_bug(bug) {
+  var assignee = ' (' + bug.assigned_to.name + ')';
+  if (bug.assigned_to.name === 'nobody@mozilla.org') {
+    assignee = '';
+  }
+  var item = '<li id="bug_' + bug.id + '">';
+  item += '[<a href="http://bugzil.la/' + bug.id + '">' +
+          bug.id + '</a>] ';
+  if (assignee !== '') {
+    item += bug.summary + assignee;
+  } else {
+    item += '<strong>' + bug.summary + '</strong>';
+  }
+  item += '</li>\n';
+  return item;
+}
 
 // init
 jQuery(document).ready(function($) {
@@ -57,15 +73,13 @@ jQuery(document).ready(function($) {
     }
   });
 
-  /*bugzilla.getBug(678223, function(error, bug) {
+/*bugzilla.getBug(678223, function(error, bug) {
 if (!error) {
   $('#panel').text(bug.summary);
 }
 });*/
 
-var tef_bugs;
-// blockers: tef+, not npotb
-bugzilla.searchBugs({
+var params = {
     'username': 'autonome+bztest@gmail.com',
     'password': 'bztest1A',
     'bug_status': ['NEW', 'UNCONFIRMED', 'ASSIGNED', 'REOPENED', 'READY'],
@@ -86,61 +100,56 @@ bugzilla.searchBugs({
                   'Gaia::Video', 'General'
                  ],
     'product': 'Boot2Gecko'
-  }, function(error, bugs) {
+  };
+
+var tef_bugs;
+// blockers: tef+, not npotb
+bugzilla.searchBugs(params, function(error, bugs) {
 if (!error) {
   tef_bugs = bugs;
   $('#tef_panel').hide();
   // console.log(bugs);
+  var nobody_cnt = 0;
   var outcome = '<ul>';
   for (var i = 0; i < bugs.length; i++) {
-    outcome += '<li>' + '[<a href="http://bugzil.la/' + bugs[i].id + '">' +
-               bugs[i].id + '</a>] ' + bugs[i].summary + '</li>\n';
+    if (bugs[i].assigned_to.name === 'nobody@mozilla.org') {
+      nobody_cnt += 1;
+    }
+    outcome += format_bug(bugs[i]);
   }
   outcome += '</ul>';
   $('#tef_panel').html(outcome);
   $('#tef_cnt').text(bugs.length);
+  $('#tef_nobody_cnt').text('not assigned: ' + nobody_cnt);
 }
 });
 
 var leo_bugs;
+leo_params = JSON.parse(JSON.stringify(params));
+leo_params['value0-0-0'] = 'leo+';
 // blockers: leo+, not npotb
-bugzilla.searchBugs({
-    'username': 'autonome+bztest@gmail.com',
-    'password': 'bztest1A',
-    'bug_status': ['NEW', 'UNCONFIRMED', 'ASSIGNED', 'REOPENED', 'READY'],
-    'field0-0-0': 'cf_blocking_b2g',
-    'type0-0-0': 'contains',
-    'value0-0-0': 'leo+',
-    'field1-0-0': 'whiteboard',
-    'type1-0-0': 'not_contains',
-    'value1-0-0': 'npotb',
-    'component': ['Gaia', 'Gaia::Bluetooth File Transfer', 'Gaia::Browser',
-                  'Gaia::Calculator', 'Gaia::Camera', 'Gaia::Clock',
-                  'Gaia::Contacts', 'Gaia::Cost Control', 'Gaia::Dialer',
-                  'Gaia::E-Mail', 'Gaia::Everything.me',
-                  'Gaia::First Time Experience', 'Gaia::FMRadio',
-                  'Gaia::Gallery', 'Gaia::Homescreen',
-                  'Gaia::Keyboard', 'Gaia::Music', 'Gaia::PDF Viewer',
-                  'Gaia::Settings', 'Gaia::SMS', 'Gaia::System::Lockscreen',
-                  'Gaia::Video', 'General'
-                 ],
-    'product': 'Boot2Gecko'
-  }, function(error, bugs) {
+bugzilla.searchBugs(leo_params, function(error, bugs) {
 if (!error) {
   leo_bugs = bugs;
   $('#leo_panel').hide();
   // console.log(bugs);
+  var nobody_cnt = 0;
   var outcome = '<ul>';
   for (var i = 0; i < bugs.length; i++) {
-    outcome += '<li>' + '[<a href="http://bugzil.la/' + bugs[i].id + '">' +
-               bugs[i].id + '</a>] ' + bugs[i].summary + '</li>\n';
+    if (bugs[i].assigned_to.name === 'nobody@mozilla.org') {
+      nobody_cnt += 1;
+    }
+    outcome += format_bug(bugs[i]);
   }
   outcome += '</ul>';
   $('#leo_panel').html(outcome);
   $('#leo_cnt').text(bugs.length);
+  $('#leo_nobody_cnt').text('not assigned: ' + nobody_cnt);
 }
 });
 
+/*
+// get week start and end date
 var now = Date();
 var startDay = 1; //0=sunday, 1=monday etc.
 var d = now.getDay(); //get the current day
@@ -150,7 +159,7 @@ var weekStart = new Date(now.valueOf() -
                         d - startDay) * 86400000); //rewind to start day
 var weekEnd = new Date(weekStart.valueOf() +
                        6 * 86400000); //add 6 days to get last day
-
+*/
 
 /*
 //blockers-tef-no-npotb-before-workweek
